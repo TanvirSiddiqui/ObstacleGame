6e1387e258c7ea7c9d8d73314fb8465c537555f2
Md. Tanvir Siddiqui <babatanvir@hotmail.com>
2021-03-16 02:44:00 +0600

    Obstacle avoiding game

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, I am a table in unity game scene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]float speed = 2.0f;
+    int noOfHit = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal")*Time.deltaTime*speed;
+        float z = Input.GetAxis("Vertical")*Time.deltaTime*speed;
+
+        transform.Translate(x, 0, z);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Hit")
+        {
+            noOfHit++;   // noOfhit= noOfHit+1 
+            Debug.Log("collided with " + collision.gameObject.name);
+            Debug.Log("Number of hit: " + noOfHit);
+            collision.gameObject.tag = "Hit";
+        }
+   
+    }
+}
